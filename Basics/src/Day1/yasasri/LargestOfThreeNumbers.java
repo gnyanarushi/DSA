a786cb5ab71881e54534196c0a24fa11455b7c4c
yasasri61 <yasasrichowdary22@gmail.com>
2025-11-28 15:49:54 +0530

    LargestOfThreeNum

--- Basics/src/Day1/yasasri/LargestOfThreeNumbers.java
+++ Basics/src/Day1/yasasri/LargestOfThreeNumbers.java
@@ -15,5 +15,14 @@ public class LargestOfThreeNumbers {
         System.out.println("3st num:");
         int c = in.nextInt();
 
+        if (a>b && a>c){
+            System.out.println("a is largest");
+        }
+        else if(b>a && b>c){
+            System.out.println("b is largest");
+        }
+        else{
+            System.out.println("c is largest");
+        }
     }
 }
