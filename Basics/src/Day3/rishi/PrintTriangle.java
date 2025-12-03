8b792d5b08aaf2497e0a71d412c9315a816f5805
rushinandigam <rushi.nandigam@sasi.ac.in>
2025-12-03 13:25:28 +0530

    resources readme is addded

--- Basics/src/Day3/rishi/PrintTriangle.java
+++ Basics/src/Day3/rishi/PrintTriangle.java
@@ -15,5 +15,13 @@ public class PrintTriangle {
 
          */
 
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
