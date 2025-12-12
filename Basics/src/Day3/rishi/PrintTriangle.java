51c62aec43ff3792e776a32d0dc02f9904a37781
rushinandigam <rushi.nandigam@sasi.ac.in>
2025-12-13 00:28:21 +0530

    triangle programme added

--- Basics/src/Day3/rishi/PrintTriangle.java
+++ Basics/src/Day3/rishi/PrintTriangle.java
@@ -2,26 +2,75 @@ package Day3.rishi;
 
 public class PrintTriangle {
     public  static void main(String[] args){
-
-        /*
-                *
-              *  *
-             *      *
-           *          *
-         *              *
-        ******************
+        printCrone();
+    }
 
 
 
-         */
+    public static void printCrone(){
+        int n = 5 ;
+
+        for(int i = 1 ; i <= n ; i++ ){
 
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print("*");
+            for(int j = 1 ; j <= i ; j++ ){
+                System.out.print("* ");
+            }
+
+            for(int j = 1 ; j <= 2*(n-i) ; j++){
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+
+
+
+        }
+    }
+
+
+    public static void printLeftRightAngleTriangle() {
+
+        int n = 5 ;
+
+        for(int i = 1; i <= n ; i++ ){
+            for (int j = 1; j <= i; j++) {
+                System.out.print("  ");
+            }
+        }
+    }
+
+
+    public static void printRightrRightAngleTriangle() {
+        int n = 5 ;
+
+        for(int i = 1 ; i <= n; i++ ) {
+
+
+            for(int j = 1 ; j <= (n-i) ; j++ ){
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i  ; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
+    }
 
+
+
+    public static void  printSquare() {
+        int n = 5 ;
+        for(int i  = 1 ; i <= n ; i++ ) {
+            for(int j = 1 ; j <= n ; j++ ){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
