0782cc5fc4ce9427c2e32167dcbbb77ae7ad643e
gnyanarushi <gnyanarushi@gmail.com>
2026-03-16 15:02:01 +0530

    main branched merged with the ubuntu system changes

---
+++ BasicMath/src/basics/Swap.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Swap {
+    public static void main(String[] args) {
+
+
+        int a = 10 ;
+        int b = 20;
+        swap(a ,b );
+        System.out.println(a + " " + b );
+
+    }
+
+    private static void swap(int a, int b) {
+        int temp =  a;
+        a = b ;
+        b = temp;
+    }
+}
